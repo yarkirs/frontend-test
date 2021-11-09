932a7b48ac351acde8b490008d3a3a86aa3bc2f7
Яромир Амолаев <amolaev.y@akme.solar>
2021-11-09 10:31:04 +0300

    add FronEnd from my pet project

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+	let count = 0;
+	$: double = count * 2;
+
+	function countClick () {
+		count++
+		
+	}
+
+	$: {
+		console.log(double + count)
+		
+	}
+</script>
+
+<button on:click={countClick}>Ты нажал {count} раз а это повтор кода {double}</button>
+
+
